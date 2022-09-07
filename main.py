f13c0de0ff12d4db861445f63da4493a35c5a9e0
imahdimir <imahdimir@gmail.com>
2022-09-08 01:54:41 +0430

    v4

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 import pandas as pd
 from githubdata import GithubData
 from mirutil.df_utils import save_df_as_a_nice_xl as sxl
+from persiantools.characters import ar_to_fa
 
 
 class GDUrl :
@@ -17,20 +18,31 @@ gdu = GDUrl()
 class ColName :
     ftic = 'FirmTicker'
     tic = 'Ticker'
+    faftic = 'faFirmTicker'
 
 c = ColName()
 
 ptrns = {
-        0    : lambda x : x ,
-        1    : lambda x : x + '1' ,
-        2    : lambda x : x + '2' ,
-        3    : lambda x : x + '3' ,
-        4    : lambda x : x + '4' ,
-        'h'  : lambda x : x + 'ح' ,
-        'h1' : lambda x : x + 'ح' + '1' ,
-        'h2' : lambda x : x + 'ح' + '2' ,
-        'h3' : lambda x : x + 'ح' + '3' ,
-        'h4' : lambda x : x + 'ح' + '4' ,
+        0     : lambda x : x ,
+        'f0'  : lambda x : ar_to_fa(x) ,
+        1     : lambda x : x + '1' ,
+        'f1'  : lambda x : ar_to_fa(x) + '1' ,
+        2     : lambda x : x + '2' ,
+        'f2'  : lambda x : ar_to_fa(x) + '2' ,
+        3     : lambda x : x + '3' ,
+        'f3'  : lambda x : ar_to_fa(x) + '3' ,
+        4     : lambda x : x + '4' ,
+        'f4'  : lambda x : ar_to_fa(x) + '4' ,
+        'h'   : lambda x : x + 'ح' ,
+        'fh'  : lambda x : ar_to_fa(x) + 'ح' ,
+        'h1'  : lambda x : x + 'ح' + '1' ,
+        'fh1' : lambda x : ar_to_fa(x) + 'ح' + '1' ,
+        'h2'  : lambda x : x + 'ح' + '2' ,
+        'fh2' : lambda x : ar_to_fa(x) + 'ح' + '2' ,
+        'h3'  : lambda x : x + 'ح' + '3' ,
+        'fh3' : lambda x : ar_to_fa(x) + 'ح' + '3' ,
+        'h4'  : lambda x : x + 'ح' + '4' ,
+        'fh4' : lambda x : ar_to_fa(x) + 'ح' + '4' ,
         }
 
 def main() :
@@ -55,9 +67,11 @@ def main() :
         df = pd.concat([df , _df])
 
     ##
+    df.drop_duplicates(inplace = True)
+    ##
     msk = df.duplicated(subset = c.tic , keep = False)
     df1 = df[msk]
-
+    ##
     df = df[~ msk]
     ##
     df.sort_values(by = [c.ftic , c.tic] , inplace = True)
